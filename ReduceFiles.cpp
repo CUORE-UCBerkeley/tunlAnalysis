3211db8bc8f8e68f45ccd84265d491a74f1341f5
Vivek Singh <singhv@berkeley.edu>
2022-01-20 00:17:20 -0800

    started on RDataFrame for analysis

--- ReduceFiles.cpp
+++ ReduceFiles.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <vector>
+#include <getopt.h>
+#include <stdio.h>
 
 #include "TChain.h"
 #include "TCanvas.h"
@@ -7,125 +9,143 @@
 #include "TH1F.h"
 #include "TSpectrum.h"
 #include <ROOT/RDataFrame.hxx>
-#include "TLine.h"
+#include <ROOT/RVec.hxx>
 
-void Calibrate( TH1D *hist){
+using namespace ROOT::VecOps;
+using RvecD = ROOT::RVec<double>;
 
-    //Find peaks in the histogram for calibration
-    // Use TSpectrum to find the peak candidates
-    int npeaks = 30;
-    TSpectrum *s = new TSpectrum(2*npeaks);
-    int nfound = s->Search(hist,3,"",0.1);
-    printf("Found %d candidate peaks to fit\n",nfound);
-    // Estimate background using TSpectrum::Background
-    TH1 *hb = s->Background(hist,20,"same");
-    //if (hb) c1->Update();
-
+void print_usage() {
+  std::cout << "Usage: ./Reduce --material c --energy i" << "\n" 
+            << "Choose from the following : " << "\n"  
+            << "material: {MoFoil,MoPowder,Fe,Cu,Empty}" << "\n" 
+            << "energy: {0,4,6,8}" <<  "\n" 
+            << "Note: Not all combinations are possible. Refer to spreadsheet." << "\n";
 }
 
-int main(){
+int main(int argc, char **argv){
+
+    std::string matname;
+    double energy;
+    double energies[4]={0.,4.3,5.9,8};
+    const char* pnames[5] = {"MoFoil","MoPowder","Fe","Cu","Blank"};
+
+    static struct option long_options[] = {
+        {"material",   required_argument, 0, 'c'  },
+        {"energy"  ,   required_argument, 0, 'p'  },
+    };
+
+    int long_index =0;
+    int opt;
+
+    while ((opt = getopt_long_only(argc, argv,"", long_options, &long_index )) != -1) {
+        switch (opt) {
+        case 'c' :
+            matname = std::string(optarg);
+            break;
+        case 'p' :
+            energy = atoi(optarg);
+            break;
+        default: print_usage();
+                exit(EXIT_FAILURE);
+        }
+    }
+
+    std::cout << energy << std::endl;
+    //Create a vector of run numbers 
+    std::vector<int> runnumbers;
+    std::string nameID;
+    
+    if(matname.compare("MoFoil") ==0 && energy == 6){
+        runnumbers = {58, 59, 60, 61, 62, 63, 64, 65, 66};
+        nameID = "_MoFoil_6MeV";
+    } else if(matname.compare("FeFoil") ==0 && energy == 6){
+        runnumbers = {67, 77, 78};
+        nameID = "_FeFoil_6MeV";
+    } else if(matname.compare("MoPowder") ==0 && energy == 0){
+        runnumbers = {68, 72, 76, 85};
+        nameID = "_MoFoil_NoBeam";
+    } else if(matname.compare("MoPowder") ==0 && energy == 6){
+        runnumbers = {69, 70, 71, 73, 74, 75};
+        nameID = "_MoPowder_6MeV";
+    } else if(matname.compare("Empty") ==0 && energy == 6){
+        runnumbers = {79};
+        nameID = "_Empty_6MeV";
+    } else if(matname.compare("Cu") ==0 && energy == 6){
+        runnumbers = {80};
+        nameID = "_Cu_6MeV";
+    } else if(matname.compare("MoPowder") ==0 && energy == 8){
+        runnumbers = {81, 82, 83, 84, 86};
+        nameID = "_MoPowder_8MeV";
+    } else if(matname.compare("FeFoil") ==0 && energy == 8){
+        runnumbers = {87};
+        nameID = "_FeFoil_8MeV";
+    } else if(matname.compare("FeFoil") ==0 && energy == 4){
+        runnumbers = {88};
+        nameID = "_FeFoil_4MeV";
+    } else if(matname.compare("MoPowder") ==0 && energy == 4){
+        runnumbers = {89,90};
+        nameID = "_MoPowder_4MeV";
+    } else if(matname.compare("Empty") ==0 && energy == 4){
+        runnumbers = {91};
+        nameID = "_Empty_4MeV";
+    }
+
+    runnumbers = {58};
+    if(runnumbers.size()==0){
+        std::cout << "Please pass correct arguments with material and neutron energy." << std::endl ;
+        exit(EXIT_FAILURE);
+    }
 
     //TApplication to diaplay output on screen
     TApplication app("app",0,0);
 
-    //Store the tun numbers for Mo foil in a vector
-    std::vector<int> runMoFoil{58,59,60,61,62,63,64,65,66,67};
-    //std::vector<int> runMoFoil{67};
     
     //Add the files in a chain; tree name is SSA
-    TChain *chain = new TChain("SSA");
-    for(auto &run: runMoFoil){
-        chain->Add(Form("../root/root_data_SSA_%03d.bin_tree.root",run));
+    TChain chain("SSA");
+    for(auto &run: runnumbers){
+        chain.Add(Form("../root/root_data_SSA_%03d.bin_tree.root",run));
     }
 
-    std::cout << "The total number of entries in the root files are: " << chain->GetEntries() << std::endl;
+    std::cout << chain.GetEntries() << std::endl;
 
-    //Create canvas for diagnostic plots 
-    auto c1 = new TCanvas("c1","c1", 1200, 800);
-    c1->cd();
+    /////// Try something new ////
+    ROOT::EnableImplicitMT();
+    ROOT::RDataFrame df(chain);
+    auto h = df.Define("energy0", [](const RvecD &amplitude) { return amplitude[2]; }, {"amplitude"}).Histo1D("energy0");
+    h->Draw();
+
+    auto cutTOF = [](const RvecD ChannelTimes, const RvecD TriggerTimes){
+        return (ChannelTimes[0] - TriggerTimes[0]);
+    };
+    auto hTOF = df.Define("TOF_ch0", cutTOF, {"channel_time","trigger_time"})
+                  . Histo1D({"name", "title", 1000, -4000, 0},"TOF_ch0");
+    hTOF->Draw();
+
+    //////////////////////////////
+
+    // //Create canvas for diagnostic plots 
+    // auto c1 = new TCanvas("c1","c1", 1200, 800);
+    // c1->Divide(1,4);
    
-    //Take the raw amplitudes and put them in histograms
-    
-    std::vector<TH1D *> hAmpRaw; //contains raw amplitude spectra
-    std::vector<TH1D *> hToF; //contans Time of flight spectra
-    std::vector<TH1D *> hAmpToFCorr; //contains ToF corrected amplitude spectra
-    std::vector<TH1D *> hAmpToFPrompt; //contains time of flight corrected amplitude spectra
-    std::vector<TH1D *> hAmpToFEarly; //contains accidental amplitude spectra on Early
-    std::vector<TH1D *> hAmpToFLate; //contans accidental amplitude spectra on Late
-    
-    gPad->Print("diagnosticPlots.pdf(");
-        
-    for(int i = 0; i < 4; i++){
+    // //Take the raw amplitudes and put them in histograms
+    // std::vector<TH1F> hAmplitude;
+    // for(int i = 0; i < 4; i++){
+    //     c1->cd(i+1);
+    //     TH1F *h = new TH1F(Form("h%d",2*i),Form("h%d",2*i),10000,0,60000);
+    //     chain.Draw(Form("amplitude[0]:amplitude[2]:amplitude[4]:amplitude[6] \
+    //                      >>h%d",(2*i),(2*i)),"","goff");
+    //     hAmplitude.push_back(*h);
+    // }
 
-        int kk = 2 * i; //Takes care of the fact that detector numbers are 0, 2, 4, 6
-
-        //Fill in the raw spectra; We will use it for calibration
-        hAmpRaw.push_back(new TH1D(Form("hAmpRaw%d", kk),Form("hAmpRaw%d", kk),10000,0,60000));
-        chain->Draw(Form("amplitude[%d]>>hAmpRaw%d",kk,kk),"", "goff");
-
-        // //Fill in the time of flight spectra & stick with main peak only for now
-        // hToF.push_back(new TH1D(Form("hToF%d",kk),Form("hToF%d",kk), 1000, -4000, 0));
-        // chain->Draw(Form("(channel_time[%d] - trigger_time[0])>>hToF%d",kk,kk), \
-        //             Form("(channel_time[%d] - trigger_time[0]) > -4000", kk),"goff"); 
-        // //Add lines for where the ToF cut will be made
-        // hToF[i]->Draw();
-        // double ymax = hToF[i]->GetMaximum();;
-        // int binmax = hToF[i]->GetMaximumBin(); 
-        // double xmax = hToF[i]->GetXaxis()->GetBinCenter(binmax);
-        // TLine *line1 = new TLine(xmax - 500, 0, xmax - 500,ymax);
-        // line1->SetLineColor(kRed);
-        // line1->Draw();
-        // TLine *line2 = new TLine(xmax + 500, 0, xmax + 500,ymax);
-        // line2->SetLineColor(kRed);
-        // line2->Draw();
-        // gPad->Print("diagnosticPlots.pdf");
-
-        // //Fill in the prompt spectra
-        // hAmpToFPrompt.push_back(new TH1D(Form("hAmpToFPrompt%d", kk),Form("hAmptToFPrompt%d", kk),10000,0,60000));
-        // chain->Draw(Form("amplitude[%d]>>hAmpToFPrompt%d",kk,kk),
-        //             Form("(channel_time[%d] - trigger_time[0]) > %f && (channel_time[%d] - trigger_time[0]) < %f ", kk, xmax - 500, kk, xmax + 500), \
-        //             "goff");
-        // double cutRangePrompt = abs( (xmax + 500) - (xmax - 500) );
-        // std::cout << "cutRangePrompt =  " << cutRangePrompt << "\n";
-        // hAmpToFPrompt[i]->Draw(); 
-        // gPad->Print("diagnosticPlots.pdf"); 
-
-        // // Get the early accidental spectra 
-        // hAmpToFEarly.push_back(new TH1D(Form("hAmpToFEarly%d", kk),Form("hAmptToFEarly%d", kk),10000,0,60000));
-        // chain->Draw(Form("amplitude[%d]>>hAmpToFEarly%d",kk,kk),
-        //             Form("(channel_time[%d] - trigger_time[0]) > -4000 && (channel_time[%d] - trigger_time[0]) < %f ", kk, kk, xmax - 500), \
-        //             "goff");
-        // double cutRangeEarly = abs(-4000 - (xmax -500));
-        // std::cout << "cutRangeEarly =  " << cutRangeEarly << "\n";
-        // hAmpToFEarly[i]->Draw(); 
-        // gPad->Print("diagnosticPlots.pdf");           
-
-        // // Get the early accidental spectra 
-        // hAmpToFLate.push_back(new TH1D(Form("hAmpToFLate%d", kk),Form("hAmpToFLate%d", kk),10000,0,60000));
-        // chain->Draw(Form("amplitude[%d]>>hAmpToFLate%d",kk,kk), \
-        //             Form("(channel_time[%d] - trigger_time[0]) < 0 && (channel_time[%d] - trigger_time[0]) > %f ", kk, kk, xmax + 500), \
-        //             "goff");
-        // double cutRangeLate = abs(0 - (xmax + 500));
-        // std::cout << "cutRangeLate =  " << cutRangeLate << "\n";
-        // hAmpToFLate[i]->Draw(); 
-        // gPad->Print("diagnosticPlots.pdf");
-
-        // // Fill in the ToF corrected amplitude spectra
-        // hAmpToFCorr.push_back(new TH1D(Form("hAmpToF%d",kk),Form("hAmpToF%d", kk),10000,0,60000));
-        // // The chosen width for ToF prompt spectra is 1000 (500 + 500), so we will normalize the early and late spectra to that
-        // hAmpToFCorr[i]->Add(hAmpToFPrompt[i], 1.);
-        // hAmpToFCorr[i]->Add(hAmpToFEarly[i], - 0.5*cutRangePrompt/cutRangeEarly);
-        // hAmpToFCorr[i]->Add(hAmpToFLate[i] , - 0.5*cutRangePrompt/cutRangeLate);
-        // hAmpToFCorr[i]->Draw(); 
-        // gPad->Print("diagnosticPlots.pdf");
-
-    }
-
-    c1->Clear();
-    gPad->Print("diagnosticPlots.pdf)");
-
-    Calibrate(hAmpRaw[0]);
-
+    // //Find peaks in the histogram for calibration
+    // // Use TSpectrum to find the peak candidates
+    // int npeaks = 20;
+    // TSpectrum *s = new TSpectrum(2*npeaks);
+    // int nfound = s->Search(&hAmplitude[0],2,"",0.005);
+    // printf("Found %d candidate peaks to fit\n",nfound);
+    // // Estimate background using TSpectrum::Background
+    // TH1 *hb = s->Background(&hAmplitude[0],20,"same");
+    // if (hb) c1->Update();
     
 
     // c1->SaveAs("diagnosticPlots.pdf");
